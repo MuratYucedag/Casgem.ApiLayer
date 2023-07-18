69f6794942837ef066191ee16ea57713b0b9b6ce
MuratYucedag <yucedagmurat23@gmail.com>
2023-07-18 14:25:23 +0300

    Add project files.

---
+++ Casgem.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using Casgem.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casgem.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-R7AR1ND;initial catalog=CasgemApiDb;integrated security=true;");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Casgem.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using Casgem.DataAccessLayer.Abstract;
+using Casgem.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casgem.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly Context _context;
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+        public void Delete(T t)
+        {
+            //_context.
+        }
+
+        public T GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
